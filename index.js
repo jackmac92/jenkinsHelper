53d73f7dce5f0af2902a42fa2ef244e5a0dfce84
Jack McCown <jmccown@cbinsights.com>
2017-05-23 11:26:18 -0400

    updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import jenkins from 'jenkins';
-
+// TODO figure out how to determine if build is active
 const password = '2zxx8b4Pba7u';
 const username = 'jmccown';
 
@@ -10,52 +10,65 @@ const Jenkins = jenkins({
   promisify: true
 });
 
-const formatJobReport = (jobReport, jobName) =>
-  Jenkins.build.get(jobName, jobReport.lastBuild.number).then(lastBuild => {
-    const buildNowUrl = `${jenkinsBaseUrl}/${jobReport.url.split('jenkins.cbinsights.com')[1]}/build?delay=0sec`;
-    return {
-      text: jobName
-        .split('/')
-        .map(x => x[0].toUpperCase() + x.slice(1))
-        .join(' '),
-      color: lastBuild.result === 'SUCCESS' ? 'green' : 'red',
-      submenu: [
-        {
-          text: 'Build Now',
-          bash: 'curl',
-          param1: '-X',
-          param2: 'POST',
-          param3: buildNowUrl,
-          terminal: false,
-          color: 'blue'
-        },
-        // { text: 'current build' },
-        {
-          text: 'Most Recent Build',
-          href: jobReport.lastBuild.url,
-          color: lastBuild.result === 'SUCCESS' ? 'green' : 'red'
-        },
-        {
-          text: 'Last Failure',
-          href: jobReport.lastFailedBuild.url,
-          color: 'red'
-        },
-        {
-          text: 'Last Successful Build',
-          href: jobReport.lastSuccessfulBuild.url,
-          color: 'green'
-        },
-        {
-          text: 'Full History',
-          submenu: jobReport.builds.map(b => ({
-            text: `${jobName} build ${b.number}`,
-            href: b.url,
-            size: 8
-          }))
-        }
-      ]
-    };
-  });
+const getBuildHistory = (jobReport, jobName, numHist) =>
+  Promise.all(
+    jobReport.builds
+      .slice(0, numHist)
+      .map(b =>
+        Jenkins.build.get(jobName, b.number).catch(err => console.log(err))
+      )
+  ).then(buildHistory => ({
+    ...jobReport,
+    buiilds: buildHistory,
+    buildNowUrl: `${jenkinsBaseUrl}/${jobReport.url.split('jenkins.cbinsights.com')[1]}/build?delay=0sec`
+  }));
+
+const getJobInfo = (jobName, numHist = 5) =>
+  Jenkins.job
+    .get(jobName)
+    .then(jobReport => getBuildHistory(jobReport, jobName, numHist));
+
+const formatJobReport = jobReport => ({
+  text: jobReport.name,
+  color: jobReport.builds[0].result === 'SUCCESS' ? 'green' : 'red',
+  submenu: [
+    // { text: 'current build' },
+    {
+      text: 'Build Now',
+      bash: 'curl',
+      param1: '-X',
+      param2: 'POST',
+      param3: jobReport.buildNowUrl,
+      terminal: false,
+      color: 'blue'
+    },
+    {
+      text: 'Most Recent Build',
+      href: jobReport.builds[0].url,
+      color: jobReport.builds[0].result === 'SUCCESS' ? 'green' : 'red'
+    },
+    {
+      text: 'Last Failure',
+      href: jobReport.lastFailedBuild.url,
+      color: 'red'
+    },
+    {
+      text: 'Last Successful Build',
+      href: jobReport.lastSuccessfulBuild.url,
+      color: 'green'
+    },
+    {
+      text: 'Full History',
+      submenu: jobReport.builds.map(b => ({
+        text: `Build ${b.number}`,
+        href: b.url,
+        size: 8
+      }))
+    }
+  ]
+});
+const getJobInfoWithFormat = jobName =>
+  getJobInfo(jobName).then(formatJobReport);
 
 const getJobReports = jobs =>
   Promise.all(jobs.map(j => j.jenkinsName).map(Jenkins.job.get));
@@ -63,16 +76,4 @@ const getJobReports = jobs =>
 export const getJobReport = Jenkins.job.get;
 
 export const getFormattedJobReports = jobs =>
-  new Promise(resolve => {
-    Promise.all(jobs.map(j => Jenkins.job.get(j.jenkinsName))).then(reports => {
-      const problemJobs = reports.filter(r => r.healthReport.score < 90);
-      let err;
-      if (problemJobs.length) {
-        err = problemJobs.map(j => j.name).join(' | ');
-      }
-      const formatPromises = reports.map((r, idx) =>
-        formatJobReport(r, jobs[idx].jenkinsName)
-      );
-      Promise.all(formatPromises).then(items => resolve({ err, items }));
-    });
-  });
+  jobs.map(j => j.jenkinsName).map(getJobInfoWithFormat);
