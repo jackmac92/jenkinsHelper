4f34176ebc78b4a3e4ce0e319748289f9f894ae9
Jack McCown <jmccown@cbinsights.com>
2017-05-23 13:49:17 -0400

    Post publish commit

--- bundle.js
+++ bundle.js
@@ -36,48 +36,4 @@ const getJobInfo = (jobName, numHist = 5) =>
     .get(jobName)
     .then(jobReport => getBuildHistory(jobReport, jobName, numHist));
 
-const formatJobReport = jobReport => ({
-  text: jobReport.name,
-  color: jobReport.builds[0].result === 'SUCCESS' ? 'green' : 'red',
-  submenu: [
-    // { text: 'current build' },
-    {
-      text: 'Build Now',
-      bash: 'curl',
-      param1: '-X',
-      param2: 'POST',
-      param3: jobReport.buildNowUrl,
-      terminal: false,
-      color: 'blue'
-    },
-    {
-      text: 'Most Recent Build',
-      href: jobReport.builds[0].url,
-      color: jobReport.builds[0].result === 'SUCCESS' ? 'green' : 'red'
-    },
-    {
-      text: 'Last Failure',
-      href: jobReport.lastFailedBuild.url,
-      color: 'red'
-    },
-    {
-      text: 'Last Successful Build',
-      href: jobReport.lastSuccessfulBuild.url,
-      color: 'green'
-    },
-    {
-      text: 'Full History',
-      submenu: jobReport.builds.map(b => ({
-        text: b.fullDisplayName,
-        href: b.url,
-        color: b.result === 'SUCCESS' ? 'green' : 'red',
-        size: 8
-      }))
-    }
-  ]
-});
-
-getJobInfo('cbi-site/develop').then(res => console.log(res.buildHistory));
-
 exports.getJobInfo = getJobInfo;
-exports.formatJobReport = formatJobReport;
