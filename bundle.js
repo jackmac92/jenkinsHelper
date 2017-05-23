f64f51f4085ae47773d390298c14a0626017554a
Jack McCown <jmccown@cbinsights.com>
2017-05-23 13:53:38 -0400

    Post publish commit

--- bundle.js
+++ bundle.js
@@ -24,16 +24,17 @@ const getBuildHistory = (jobReport, jobName, numHist) =>
       .map(b =>
         Jenkins.build.get(jobName, b.number).catch(err => console.log(err))
       )
-  ).then(buildHistory =>
-    Object.assign({}, jobReport, {
-      buildHistory,
-      buildNowUrl: `${jenkinsBaseUrl}/${jobReport.url.split('jenkins.cbinsights.com')[1]}/build?delay=0sec`
-    })
   );
 
 const getJobInfo = (jobName, numHist = 5) =>
   Jenkins.job
     .get(jobName)
-    .then(jobReport => getBuildHistory(jobReport, jobName, numHist));
+    .then(jobReport => getBuildHistory(jobReport, jobName, numHist))
+    .then(buildHistory =>
+      Object.assign({}, jobReport, {
+        buildHistory,
+        buildNowUrl: `${jenkinsBaseUrl}/${jobReport.url.split('jenkins.cbinsights.com')[1]}/build?delay=0sec`
+      })
+    );
 
 exports.getJobInfo = getJobInfo;
